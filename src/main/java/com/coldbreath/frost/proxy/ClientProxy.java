7e22585d6757ffa7fdeef0b3e7620b572da1e859
방세준 <71654451+bangsejun@users.noreply.github.com>
2020-11-06 21:29:45 +0900

    Add files via upload

---
+++ src/main/java/com/coldbreath/frost/proxy/ClientProxy.java
@@ -0,0 +1,11 @@
+package com.coldbreath.frost.proxy;
+
+import net.minecraft.item.Item;
+
+    public class ClientProxy extends CommonProxy {
+        public ClientProxy() {
+        }
+
+        public void registerRenderers(Item item, int meta, String id) {
+        }
+    }
---
+++ src/main/java/com/coldbreath/frost/proxy/CommonProxy.java
@@ -0,0 +1,11 @@
+package com.coldbreath.frost.proxy;
+
+import net.minecraft.item.Item;
+
+public class CommonProxy {
+    public CommonProxy() {
+    }
+
+    public void registerRenderer(Item item, int meta, String id) {
+    }
+}
